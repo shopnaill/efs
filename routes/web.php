4a234175cf91887ccd7fc97aa5fccc989ef4b77b
Mohamed Mostafa <mohamed1999250@gmail.com>
2022-04-08 03:08:46 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -12,20 +12,27 @@ require_once 'admin.php';
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
- 
+ */
 
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
 // storage routes
-Route::get('/storage/{path}', function ($path) {
-    return Storage::disk('public')->get($path);
-})->where('path', '.*');
+Route::get('storage/{dir}/{dir2}/{file}', function ($dir, $dir2, $file) {
+    $path = storage_path('app' . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $dir2 . DIRECTORY_SEPARATOR . $file);
+    if (file_exists($path)) {
+        return response()->file($path);
+    } else {
+        return response()->json(['error' => 'File not found'], 404);
+    }
+});
 
-// storage routes with subfolder
-Route::get('/storage/{folder}/{path}', function ($folder, $path) {
-    return Storage::disk('public')->get($folder.'/'.$path);
-})->where('path', '.*');
+Route::get('storage/{dir}/{file}', function ($dir, $file) {
+    $path = storage_path('app' . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $file);
+    if (file_exists($path)) {
+        return response()->file($path);
+    } else {
+        return response()->json(['error' => 'File not found'], 404);
+    }
+});
